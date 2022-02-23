f662f2f8502bcebf1bb4fc18b662312776797baf
chagy <chagy_x_@hotmail.com>
2022-02-23 11:49:49 +0700

    15

--- app/Models/User.php
+++ app/Models/User.php
@@ -70,4 +70,9 @@ class User extends Authenticatable implements JWTSubject,MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new VerifyEmail);
+    }
 }

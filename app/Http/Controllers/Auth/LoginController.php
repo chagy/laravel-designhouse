6650f793f37058db355f1518b4dba4cf29148f0c
chagy <chagy_x_@hotmail.com>
2022-02-23 14:29:28 +0700

    18

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,39 +2,66 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\MustVerifyEmail;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function attemptLogin(Request $request)
     {
-        $this->middleware('guest')->except('logout');
+        $token = $this->guard()->attempt($this->credentials($request));
+
+        if(!$token){
+            return false;
+        }
+
+        $user = $this->guard()->user();
+
+        if($user instanceof MustVerifyEmail && !$user->hasVerifiedEmail()){
+            return false;
+        }
+
+        $this->guard()->setToken($token);
+
+        return true;
+    }
+
+    protected function sendLoginResponse(Request $request)
+    {
+        $this->clearLoginAttempts($request);
+
+        $token = (string)$this->guard()->getToken();
+
+        $expiration = $this->guard()->getPayload()->get('exp');
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $expiration
+        ]);
+    }
+
+    protected function sendFailedLoginResponse()
+    {
+        $user = $this->guard()->user();
+
+        if($user instanceof MustVerifyEmail && !$user->hasVerifiedEmail()){
+            return response()->json([
+                "errors" => [
+                    "verification" => "You need to verify your email account"
+                ]
+                ]);
+        }
+
+        throw ValidationException::withMessage([
+            $this->username() => "Invalid credentials"
+        ]);
     }
 }

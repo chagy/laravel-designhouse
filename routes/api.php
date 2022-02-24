3804cd89e9059fb9e4d6d6fdf1694d9a1be28939
chagy <chagy_x_@hotmail.com>
2022-02-24 10:50:20 +0700

    32

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ Route::group(['middleware' => ['auth:api']],function(){
 
     Route::put('settings/profile','User\SettingsController@updateProfile');
     Route::put('settings/password','User\SettingsController@updatePassword');
+
+    Route::post('designs','Designs\UploadController@upload');
 });
 
 Route::group(['middleware' => ['guest:api']],function(){

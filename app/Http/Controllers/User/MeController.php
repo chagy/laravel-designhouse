024fe62221fc32f64d4027127279446e44b8bb41
chagy <chagy_x_@hotmail.com>
2022-02-24 08:26:02 +0700

    22

--- app/Http/Controllers/User/MeController.php
+++ app/Http/Controllers/User/MeController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 
 class MeController extends Controller
 {
     public function getMe()
     {
         if(auth()->check()){
-            return response()->json(["user" => auth()->user()],200);
+            $user = auth()->user();
+            return new UserResource($user);
+            // $user->created_at_human = $user->created_at->diffForHumans();
+            // return response()->json(["user" => $user],200);
         }
         return response()->json(null,200);
     }

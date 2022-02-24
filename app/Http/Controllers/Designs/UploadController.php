677412d06a77254faa34bb77e408a9bb585c1b59
chagy <chagy_x_@hotmail.com>
2022-02-24 11:03:53 +0700

    33

--- app/Http/Controllers/Designs/UploadController.php
+++ app/Http/Controllers/Designs/UploadController.php
@@ -10,7 +10,24 @@ class UploadController extends Controller
     public function upload(Request $request)
     {
         $this->validate($request,[
-            'image' => ['required','mime:jpeg,gif,bmp,png','max:2048'],
+            'image' => ['required','mimes:jpeg,gif,bmp,png','max:2048'],
         ]);
+
+        $image = $request->file('image');
+        $image_path = $image->getPathName();
+
+        $filename = time()."_".preg_replace('/\$+/','_',strtolower($image->getClientOriginalName()));
+
+        $tmp = $image->storeAs('uploads/original',$filename,'tmp');
+
+        $design = auth()->user()->designs()->create([
+            'image' => $filename,
+            'disk' => config('site.upload_disk')
+        ]);
+
+
+        $this->dispatch(new UploadImage($design));
+
+        return response()->json($design,200);
     }
 }

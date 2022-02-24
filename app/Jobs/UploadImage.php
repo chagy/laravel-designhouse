9a6e8a68d05df82f834f03ff550ebd4d245fd1ce
chagy <chagy_x_@hotmail.com>
2022-02-24 11:31:25 +0700

    35

--- app/Jobs/UploadImage.php
+++ app/Jobs/UploadImage.php
@@ -2,24 +2,26 @@
 
 namespace App\Jobs;
 
+use App\Models\Design;
 use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
 class UploadImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $design;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Design $design)
     {
-        //
+        $this->design = $design;
     }
 
     /**
@@ -29,6 +31,24 @@ class UploadImage implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $disk = $this->design->disk;
+        $original_file = storage_path().'/uploads/original'.$this->design->image;
+
+        try {
+            Image::make($original_file)
+                ->fit(800,600,function($constraint){
+                    $constraint->aspectRatio();
+                })
+                ->save($large = storage_path('uploads/large/'.$this->design->image));
+
+            Image::make($original_file)
+                ->fit(250,200,function($constraint){
+                    $constraint->aspectRatio();
+                })
+                ->save($large = storage_path('uploads/thumbnail/'.$this->design->image));
+
+        }catch(\Exception $e){
+            \Log::error($e->getMessage());
+        }
     }
 }
